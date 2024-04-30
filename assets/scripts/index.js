0113ec9c415b09d3b619dc1cc7a93f085f198cb1
Philipe Frazão Santos <philipefrazao@gmail.com>
2024-04-30 20:34:01 -0300

    responsividade ok

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,16 +2,6 @@ const navbar = document.querySelector('.navbar');
 const mobileNavbar = document.querySelector('.navbar__mobile');
 let button = document.getElementById('burguer');
 
-function manipularNavbar() {
-   
-    const posicaoRolagem = window.scrollY;
-
-    if (posicaoRolagem > 0) {
-        navbar.classList.add('active');
-    } else {
-        navbar.classList.remove('active');
-    }
-}
-
-
-window.addEventListener('scroll', manipularNavbar);
+button.addEventListener('click', function () {
+    mobileNavbar.classList.toggle('active');
+});
